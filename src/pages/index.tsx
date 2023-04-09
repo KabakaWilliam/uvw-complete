5825a9a040c473f3b9fe37be016e43e3dcd663be
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2023-04-09 10:57:33 +0300

    changed height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -156,7 +156,7 @@ const ProfileMedia = () => {
     <section className="borderContainers h-full w-full overflow-y-scroll  px-[3%] py-[40px] font-sans md:w-[53%] md:border-y">
       <div className="h-max w-full border border-[#59D6F6] p-[14px]">
         <div className="flex h-[300px] w-full overflow-x-scroll md:h-[448px]">
-          {/* <Carousel items={items} /> */}
+          <Carousel items={items} />
         </div>
         <div className="h-2" />
         <div className=" w-full  px-[2%] text-[18px] text-[#C6C6C6]">
@@ -184,7 +184,7 @@ const MobileDataTable = () => {
 
   if (CurrentModal == "profileModal") {
     return (
-      <div className="absolute top-0 z-40 flex h-max  w-screen flex-col bg-[#0f3649]  md:hidden">
+      <div className="absolute top-0 z-40 flex h-screen  w-screen flex-col bg-[#0f3649]  md:hidden">
         <ProfileBio />
         <ProfileMedia />
       </div>
